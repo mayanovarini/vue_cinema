0892db6b4b818a85cbc22f7f949659604f0a9968
thegirlfromipanema <mayanovarini@gmail.com>
2018-01-04 00:30:09 -0800

    configure components for movie and filter

--- src/main.js
+++ src/main.js
@@ -3,7 +3,31 @@ import './style.scss';
 
 new Vue({
   el: '#app',
-  data: {
-    msg: 'Hello world'
+  components: {
+    'movie-list': {
+      template: `<div id="movie-list">
+                    <div v-for="movie in movies" class="movie">{{ movie.title }}</div>
+                 </div>`,
+      data() {
+        return {
+          movies: [
+            { title: 'Pulp Fiction' },
+            { title: 'Doraemon' },
+            { title: 'Jumanji' }
+          ]
+        }
+      }
+    },
+    'movie-filter': {
+      template: `<div id="movie-filter">
+                    <h2>Filter Results</h2>
+                    <check-filter></check-filter>
+                 </div>`,
+      components: {
+        'check-filter': {
+          template: `<div>Filter</div>`
+        }
+      }
+    }
   }
 });
