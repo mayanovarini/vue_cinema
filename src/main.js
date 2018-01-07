4633eb4a45e1d068b39fd6f326551a2606cb9d7f
thegirlfromipanema <mayanovarini@gmail.com>
2018-01-06 23:44:53 -0800

    add checkFilter method

--- src/main.js
+++ src/main.js
@@ -6,45 +6,51 @@ new Vue({
   el: '#app',
   components: {
     'movie-list': {
-      template: `<div id="movie-list">
-                    <div v-for="movie in movies" class="movie">{{ movie.title }}</div>
-                 </div>`,
-      data() {
-        return {
-          movies: [
-            { title: 'Pulp Fiction' },
-            { title: 'Doraemon' },
-            { title: 'Jumanji' }
-          ]
+        template: `<div id="movie-list">
+                      <div v-for="movie in movies" class="movie">{{ movie.title }}</div>
+                   </div>`,
+        data() {
+          return {
+            movies: [
+              { title: 'Pulp Fiction' },
+              { title: 'Doraemon' },
+              { title: 'Jumanji' }
+            ]
+          }
         }
-      }
     },
     'movie-filter': {
-      data() {
-        return {
-          genres
-        }
-      },
-      template: `<div id="movie-filter">
-                    <h2>Filter Results</h2>
-                    <div class="filter-group">
-                      <check-filter v-for="genre in genres" v-bind:title="genre"></check-filter>
-                    </div>
-                 </div>`,
-      components: {
-        'check-filter': {
-          data() {
-            return {
-              checked: false
+        data() {
+          return {
+            genres
+          }
+        },
+        template: `<div id="movie-filter">
+                      <h2>Filter Results</h2>
+                      <div class="filter-group">
+                        <check-filter v-for="genre in genres" v-bind:title="genre"></check-filter>
+                      </div>
+                   </div>`,
+        components: {
+          'check-filter': {
+            data() {
+              return {
+                checked: false
+              }
+            },
+            props: ['title'],
+            template: `<div class="check-filter" v-bind:class="{ active:checked }" v-on:click="checkFilter">
+                        <span class="checkbox"></span>
+                        <span class="check-filter-title">{{ title }}</span>
+                      </div>`,
+            methods: {
+              checkFilter() {
+                this.checked = !this.checked;
+                this.$emit('check-filter');
+              }
             }
-          },
-          props: ['title'],
-          template: `<div class="check-filter" v-bind:class="{ active:checked }" v-on:click="checked = !checked">
-                      <span class="checkbox"></span>
-                      <span class="check-filter-title">{{ title }}</span>
-                    </div>`
+          }
         }
-      }
-    }
+    } // close movie-filter
   }
 });
