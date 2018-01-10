bef2c66d00dfb5ceea664d57b7c60944d72c73f4
thegirlfromipanema <mayanovarini@gmail.com>
2018-01-09 23:21:30 -0800

    add the emit event on every component which are parent to check-filter

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import genres from './util/genres.js'
 
 new Vue({
   el: '#app',
+  methods: {
+    checkFilter(category, title, checked){
+      console.log (category, title, checked)
+    }
+  },
   components: {
     'movie-list': {
         template: `<div id="movie-list">
@@ -32,8 +37,8 @@ new Vue({
                       </div>
                    </div>`,
         methods: {
-          checkFilter(){
-            console.log("check filter is fired!");
+          checkFilter(category, title, checked){
+            this.$emit('check-filter', category, title, checked);
           }
         },
         components: {
@@ -51,7 +56,7 @@ new Vue({
             methods: {
               checkFilter() {
                 this.checked = !this.checked;
-                this.$emit('check-filter');
+                this.$emit('check-filter', 'genre', this.title, this.checked);
               }
             }
           }
