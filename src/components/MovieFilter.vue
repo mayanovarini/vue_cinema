9318a268edc4227a06c57cce6c242eba8a43842d
thegirlfromipanema <mayanovarini@gmail.com>
2018-01-15 00:59:08 -0800

    add comment to clarify v-for, its source and what will it be bound to

--- src/components/MovieFilter.vue
+++ src/components/MovieFilter.vue
@@ -2,6 +2,8 @@
   <div id="movie-filter">
     <h2>Filter Results</h2>
     <div class="filter-group">
+      <!-- we use v-for so we just have to type line #7 once -->
+      <!-- we use v-bind:title="genre" NOT BIND TO GENRES to bind data from genres, because each check-filter should only have one genre -->
       <check-filter v-for="genre in genres" v-bind:title="genre" v-on:check-filter="checkFilter"></check-filter>
     </div>
   </div>
