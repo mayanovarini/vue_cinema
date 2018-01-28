3d340e28ee7f304a9e2234ca98fcfcc30952ed8b
thegirlfromipanema <mayanovarini@gmail.com>
2018-01-27 23:43:48 -0800

    add filtered session method

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -9,7 +9,7 @@
           <span class="movie-rating">{{ movie.Rated }}</span>
         </div>
         <div class="movie-sessions">
-          <div v-for="session in sessions" class="session-time-wrapper">
+          <div v-for="session in filteredSession(sessions)" class="session-time-wrapper">
             <div class="session-time">
               {{ formatSessionTime(session.time) }}
             </div>
@@ -25,6 +25,11 @@
     methods: {
       formatSessionTime(raw) {
         return this.$moment(raw).format('h:mm A'); // format method displays the session data into in hour:minute AM/PM
+      },
+      filteredSession(sessions) {
+        return sessions.filter(session => {
+          return this.$moment(session.time).isSame(this.day, 'day');
+        });
       }
     }
   }
